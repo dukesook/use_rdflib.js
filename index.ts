cb0b53db6986b472cac9da529d49c93e984a1037
dukesook <devonsookhoo14@gmailcom>
2025-08-09 13:50:03 -0600

    use the vite bundler

--- index.ts
+++ index.ts
@@ -1,21 +1,22 @@
 import * as $rdf from 'rdflib';
 
-// Create a store
 const store = $rdf.graph();
 
-// Example Turtle data
 const turtleData = `
   @prefix ex: <http://example.org/> .
   ex:subject1 ex:predicate1 "Hello World" .
 `;
 
-// Base IRI (needed for parsing)
 const baseIRI = 'http://example.org/';
 
-// Parse the Turtle into the store
 $rdf.parse(turtleData, store, baseIRI, 'text/turtle');
 
-// Log all triples
-store.statements.forEach((st) => {
-  console.log(st.subject.value, st.predicate.value, st.object.value);
-});
+function logAllTriples(graph: $rdf.IndexedFormula) {
+  graph.statements.forEach((st) => {
+    console.log(st.subject.value, st.predicate.value, st.object.value);
+  });
+}
+
+logAllTriples(store);
+
+console.log('end of file');
